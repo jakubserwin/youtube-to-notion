fc0e3c99d3b900afba110f347ee1c64785a46803
jakubserwin <kuba.serwin99@gmail.com>
2023-05-24 15:47:09 +0200

    add input validation

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,27 +4,47 @@
       <h1 class="text-center text-lg md:text-xl">
         Save Youtube Video in Notion Database
       </h1>
-      <form class="mt-12 flex flex-col">
-        <input v-model="url" type="text" class="w-full rounded-md border px-4 py-2 outline-none" placeholder="Paste video URL" required>
+      <div class="mt-12 flex flex-col">
+        <input v-model="url" type="text" class="w-full rounded-md border px-4 py-2 outline-none" placeholder="Paste video URL" />
+        <p v-if="error" class="text-sm text-red-500">
+          {{ error }}
+        </p>
         <button class="mt-4 w-full self-center rounded-md bg-pink-600 px-4 py-2 font-medium text-white transition hover:bg-pink-700" @click="handleClick">
           Save
         </button>
-      </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang='ts'>
+import { z, ZodError } from 'zod'
+
 const config = useRuntimeConfig()
 
 const url = ref('')
+const error = ref('')
+
+const schema = z.string().url({ message: 'Invalid URL format' })
 
 const handleClick = (): void => {
-  useFetch(config.public.API_BASE_URL, {
-    method: 'POST',
-    body: {
-      url: url.value
+  try {
+    schema.parse(url.value)
+
+    useFetch(config.public.API_BASE_URL, {
+      method: 'POST',
+      body: {
+        url: url.value
+      }
+    })
+
+    error.value = ''
+  } catch (err: unknown) {
+    if (err instanceof ZodError) {
+      error.value = err.errors[0].message
+    } else {
+      console.error(err)
     }
-  })
+  }
 }
 </script>
